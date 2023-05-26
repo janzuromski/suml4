0a30333b70379cf4ebd4da264026a6062b469883
Jan Żuromski <zuromskijan@gmail.com>
2023-05-26 23:42:46 +0200

    Added new frog recognition app

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pickle
+from datetime import datetime
+startTime = datetime.now()
+# import znanych nam bibliotek
+
+import pathlib
+from pathlib import Path
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+filename = "model.pkl"
+model = pickle.load(open(filename,'rb'))
+# otwieramy wcześniej wytrenowany model
+
+
+def main():
+    st.set_page_config(page_title="Jaka to żaba?")
+    overview = st.container()
+    st.image("https://cdn.mos.cms.futurecdn.net/39CUYMP8vJqHAYGVzUghBX-970-80.jpg")
+    params = st.container()
+    prediction = st.container()
+
+    with overview:
+    	st.title("Co to za gatunek żaby?")
+
+    with params:
+        frog_uploader = st.file_uploader('Zdjęcie żaby')
+
+    data = [[frog_uploader]]
+    frog = model.predict(data)
+    s_confidence = model.predict_proba(data)
+
+    with prediction:
+    	st.subheader("Jaki to gatunek żaby?")
+    	st.subheader((frog))
+    	st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][frog][0] * 100))
+
+if __name__ == "__main__":
+    main()
